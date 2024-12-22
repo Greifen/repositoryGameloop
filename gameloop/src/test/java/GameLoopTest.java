92f5926506d632746bd89005316c81ebafa43355
karin <karin@DESKTOP-JFTOVL2>
2024-12-22 16:03:21 +0100

    18 Mockito

--- gameloop/src/test/java/GameLoopTest.java
+++ gameloop/src/test/java/GameLoopTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -19,6 +20,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 
 
 class GameLoopTest {
@@ -71,138 +73,50 @@ class GameLoopTest {
 
 	@Test
 	void invokesRenderAfterUpdate() {
-		TestGame testGame = new TestGame() {
-			@Override
-			public void render() {
-				assertEquals(numberOfRenders, numberOfUpdates - 1);
-				super.render();
-			}
-		};
-		TestInputHandler testInputHandler = new TestInputHandler();
+		when(testGame.isRunning()).thenReturn(true, false);
 
-		TestTimer testTimer = new TestTimer();
-		GameLoop<TestInput> uut = new GameLoop<TestInput>(testGame, testInputHandler, testTimer);
-
-		testGame.setRunning(true, false);
 		uut.run();
-		assertThat(testGame.numberOfRenders, is(1));
+		
+		InOrder inOrder = inOrder(testGame);
+		inOrder.verify(testGame).update(any());
+		inOrder.verify(testGame).render();
 	}
 
 	@Test
 	void passesInputToUpdate() {
-		TestGame testGame = new TestGame();
-		TestInputHandler testInputHandler = new TestInputHandler();
-
-		TestTimer testTimer = new TestTimer();
-		GameLoop<TestInput> uut = new GameLoop<TestInput>(testGame, testInputHandler, testTimer);
-
 		TestInput testInput = new TestInput();
-		testInputHandler.setInput(testInput);
-		testGame.setRunning(true, false);
+		when(testInputHandler.getCurrentInput()).thenReturn(testInput);
+		when(testGame.isRunning()).thenReturn(true, false);
+		
 		uut.run();
-		assertThat(testGame.updatedWithInput, is(testInput));
+		
+		verify(testGame).update(testInput);
 	}
+	
 
 	@Test
-	void shouldSkipUpdateIfLoopIsTooFast() {
-		TestGame testGame = new TestGame();
-		TestInputHandler testInputHandler = new TestInputHandler();
-
-		TestTimer testTimer = new TestTimer();
-		testTimer.setTimes(0, 1);
-		GameLoop<TestInput> uut = new GameLoop<TestInput>(testGame, testInputHandler, testTimer);
-
-//		TestInput testInput = new TestInput();
-//		testInputHandler.setInput(testInput);
-		testGame.setRunning(true, false);
+	void skipUpdateIfLoopIsTooFast() {
+		when(testGame.isRunning()).thenReturn(true, false);
+		when(testTimer.getCurrentTime()).thenReturn(0,1);
+		
 		uut.run();
-		assertThat(testGame.numberOfUpdates, is(0));
-		assertThat(testGame.numberOfRenders, is(1));
+		
+		verify(testGame, never()).update(any());
+		verify(testGame, times(1)).render();
 	}
 
 	@Test
 	void doesAdditionalUpdateIfLoopIsTooSlow() {
-		TestGame testGame = new TestGame();
-		TestInputHandler testInputHandler = new TestInputHandler();
-
-		TestTimer testTimer = new TestTimer();
-		testTimer.setTimes(0, 2 * GameLoop.FRAME_DURATION + 1);
-		GameLoop<TestInput> uut = new GameLoop<TestInput>(testGame, testInputHandler, testTimer);
-
-//		TestInput testInput = new TestInput();
-//		testInputHandler.setInput(testInput);
-		testGame.setRunning(true, false);
+		when(testGame.isRunning()).thenReturn(true, false);
+		when(testTimer.getCurrentTime()).thenReturn(0,2 * GameLoop.FRAME_DURATION);
+		
 		uut.run();
-		assertThat(testGame.numberOfUpdates, is(2));
-		assertThat(testGame.numberOfRenders, is(1));
-	}
-
-	public static class TestTimer implements Timer {
-
-		private LinkedList<Integer> times;
-
-		public TestTimer() {
-			setTimes(0, GameLoop.FRAME_DURATION, 2 * GameLoop.FRAME_DURATION, 3 * GameLoop.FRAME_DURATION,
-					4 * GameLoop.FRAME_DURATION);
-		}
-
-		public void setTimes(Integer... times) {
-			List<Integer> values = Arrays.asList(times);
-			this.times = new LinkedList<Integer>(values);
-		}
-
-		@Override
-		public int getCurrentTime() {
-			return times.poll();
-		}
-
+		
+		verify(testGame, times(2)).update(any());
+		verify(testGame, times(1)).render();
 	}
 
 	public static class TestInput {
-
-	}
-
-	public static class TestInputHandler implements InputHandler<TestInput> {
-		private GameLoopTest.TestInput testInput;
-
-		public void setInput(TestInput testInput) {
-			this.testInput = testInput;
-
-		}
-
-		@Override
-		public GameLoopTest.TestInput getCurrentInput() {
-			return testInput;
-		}
-
-	}
-
-	public static class TestGame implements Game<TestInput> {
-
-		public TestInput updatedWithInput;
-		public int numberOfRenders;
-		public int numberOfUpdates;
-		private Queue<Boolean> running;
-
-		public void setRunning(Boolean... runningValues) {
-			List<Boolean> values = Arrays.asList(runningValues);
-			this.running = new LinkedList<Boolean>(values);
-			;
-		}
-
-		public void update(TestInput input) {
-			numberOfUpdates++;
-			updatedWithInput = input;
-		}
-
-		public boolean isRunning() {
-			return running.poll();
-		}
-
-		public void render() {
-
-			numberOfRenders++;
-		}
 
 	}
 
